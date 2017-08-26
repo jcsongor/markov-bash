cf6312458124c0ccca25618ef0da0f6ebe236664
Jozsa Csongor <jozsa.csongor@gmail.com>
2017-08-26 22:32:34 +0200

    Added usage info

--- markov.sh
+++ markov.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+if [ ${#} -lt 2 ];then
+	echo "$(basename "$0") [file] [words] - shell script to generate markov chain texts 
+
+	where:
+		file	Text file to be used to generate random text
+		words	Number of words to generate"
+	exit
+fi
+if [ ! -f "${1}" ];then
+	echo "File not found: ${1}"
+	exit;
+fi
 words=`tr '\n' ' ' < $1|sed 's/[^[:alnum:][:space:]]\+//g'| tr '[:upper:]' '[:lower:]'`
 index=$((2 + RANDOM % `echo $words|wc -w`))
 pattern=`echo $words|cut -f$index-$((index+1)) -d' '`
